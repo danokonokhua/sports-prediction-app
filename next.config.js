30a5ce89170b78c6f760d027ffd86d867032ff67
Daniel Okonokhua <danokonokhua@gmail.com>
2024-11-02 14:15:20 +0700

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  output: 'export'
+};
 
-module.exports = nextConfig
+export default nextConfig;
